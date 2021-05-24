6cc5e1b47ed09687ddbab7c61bb1b8ed89c0d3f5
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2021-05-24 13:47:24 +0100

    ADD search input

--- articles.php
+++ articles.php
@@ -7,15 +7,18 @@
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         <title>BLOG</title>
     </head>
     <body>
+    
         <div class="container">
             <div class="row">
                 <div class="col-md-12 mt-5">
                 <h1 class="">Dashboard</h1>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <input type="text" id="myInput"  placeholder="Search for article..">
                         <a href="index.php" class="btn btn-primary me-md-2" type="button">Add an article</a>
                         <a href="login.php" class="btn btn-primary" type="button">Logout</a>
                     </div>
@@ -35,7 +38,7 @@
                                 <th>ACTION</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody id="myTable">
                         <?php
                             include'Model.php';
                             $model = new Model();
@@ -77,6 +80,16 @@
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-
+        
+        <script>
+            $(document).ready(function(){
+            $("#myInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#myTable tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+            });
+        </script>
     </body>
 </html>
